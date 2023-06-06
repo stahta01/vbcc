9b62123e5869fd28254726ad51cd4a9d50c7c87d
Tim Stahlhut <stahta01@gmail.com>
2023-06-06 15:22:30 -0400

    Add hc12/dt.{c,h}

---
+++ machines/hc12/dt.c
@@ -0,0 +1,38 @@
+
+
+/* Machine generated file. DON'T TOUCH ME! */
+
+
+#include <stdint.h>
+#include "dt.h"
+
+#undef BITSIZE
+#define BITSIZE 32
+#undef DTTTYPE
+#define DTTTYPE dt11t
+#undef DTFTYPE
+#define DTFTYPE dt11f
+dt11t dtcnv11f(dt11f
+#include "dtswap32f.c"
+dt11f dtcnv11t(dt11t
+#include "dtswap32t.c"
+#undef BITSIZE
+#define BITSIZE 32
+#undef DTTTYPE
+#define DTTTYPE dt12t
+#undef DTFTYPE
+#define DTFTYPE dt12f
+dt12t dtcnv12f(dt12f
+#include "dtswap32f.c"
+dt12f dtcnv12t(dt12t
+#include "dtswap32t.c"
+#undef BITSIZE
+#define BITSIZE 32
+#undef DTTTYPE
+#define DTTTYPE dt13t
+#undef DTFTYPE
+#define DTFTYPE dt13f
+dt13t dtcnv13f(dt13f
+#include "dtswap32f.c"
+dt13f dtcnv13t(dt13t
+#include "dtswap32t.c"
